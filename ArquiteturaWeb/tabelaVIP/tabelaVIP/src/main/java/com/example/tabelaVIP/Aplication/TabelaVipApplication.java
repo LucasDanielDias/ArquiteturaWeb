cd1a456f056ad73494b46764151ff506207ad419
LucasDanielDias <111770952+LucasDanielDias@users.noreply.github.com>
2024-03-01 22:11:15 -0300

    Add files via upload
    Criação Aula Arquitetura

---
+++ ArquiteturaWeb/tabelaVIP/tabelaVIP/src/main/java/com/example/tabelaVIP/Aplication/TabelaVipApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabelaVIP.Aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+public class TabelaVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaVipApplication.class, args);
+	}
+
+}
